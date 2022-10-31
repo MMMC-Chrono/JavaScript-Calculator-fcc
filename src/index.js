892445cb6131255d7d804bf28f89baadff09a8b7
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-11-01 00:12:56 +0630

    added numbers

---
+++ src/index.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#clear").click(function() {
+      $("#display").text("0");
+    });
+    //numbers
+    $("#zero").click(function() {
+      $("#display").append($("#zero").text());
+    });
+    $("#one").click(function() {
+      $("#display").append($("#one").text());
+    });
+    $("#two").click(function() {
+      $("#display").append($("#two").text());
+    });
+    $("#three").click(function() {
+      $("#display").append($("#three").text());
+    });
+    $("#four").click(function() {
+      $("#display").append($("#four").text());
+    });
+    $("#five").click(function() {
+      $("#display").append($("#five").text());
+    });
+    $("#six").click(function() {
+      $("#display").append($("#six").text());
+    });
+    $("#seven").click(function() {
+      $("#display").append($("#seven").text());
+    });
+    $("#eight").click(function() {
+      $("#display").append($("#eight").text());
+    });
+    $("#nine").click(function() {
+      $("#display").append($("#nine").text());
+    });
+
+  });
