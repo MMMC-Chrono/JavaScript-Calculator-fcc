bfd3d3a3e810498f0a376352906f1819f75b0eb8
May Mya Mya Cin <maymyamyacin@gmail.com>
2022-11-02 22:00:10 +0630

    deleted classify function

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function displayNum(num) {
   $("#display").append(num.text());
 }
 
-const operators = ["=", "+", "-", "x", "/"];
+const operators = ["+", "-", "x", "/"];
 let sum = false;
 let subtract = false;
 
@@ -17,27 +17,20 @@ let numbers = [];
 function calculation() {
   let arr = $("#display").text().split(" ");
   for (let i = 0; i < arr.length; i++) {
-    classify(arr[i]);
-  }
-}
-function classify(val) {
-  let input = operators.indexOf(val);
+    let input = operators.indexOf(arr[i]);
   if(input === -1) {
-    //console.log(val, "numbers")
-    numbers.push(val);
-  } else if (input > 0) {
-    console.log(val, "operators" , input)   
-    if (input === 2) {
-      subtract = true;
-      console.log("-")
-    } else if (input === 1) {
+    numbers.push(arr[i]);
+  } else if (input > -1) { 
+    
+    if (input === 0) {
       sum = true;
-      console.log("+")
+      console.log(i, "add")
+    } else if (input === 1) {
+      subtract = true;
+      console.log(i, "minus")
     }
 
-  } else if (input === 0) {
-    //console.log(val, "equals")
-
+  }
   }
 }
 
@@ -52,33 +45,23 @@ removeZero();
 calculation();
 let addition = 0;
 let subtraction = 0;
-console.log(sum, "sum1")
-if (sum === true) {
-//console.log(numbers, "succeed");
-for (j in numbers) {
-  addition += parseInt(numbers[j]);
-}
-//console.log(addition);    
-numbers = [];
-numbers.push(addition);    
-//console.log(numbers);
+/*if (sum === true) {
+  for (j in numbers) {
+    addition = parseInt(numbers[0]) + parseInt(numbers[1]);
+  }
+  numbers = [];
+  numbers.push(addition);   
+  sum = false;
 
 } else if (subtract === true) {
-console.log(numbers, "succeed");
-for (j in numbers) {
-  subtraction -= parseInt(numbers[j]);
-}
-//console.log(subtraction);    
-numbers = [];
-numbers.push(subtraction);    
-//console.log(numbers);
-}
-console.log(sum, "sum")
-console.log(subtract, " subtract")
+  console.log(numbers, "succeed");
+  subtraction = numbers[0] - numbers[1];   
+  numbers = [];
+  numbers.push(subtraction);  
+  subtract = false;
+}*/
 let total = addition + subtraction;
 $("#display").append(" = ", total)
-sum = false;
-subtract = false;
 });
 
 //numbers
